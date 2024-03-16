558b2cbe90ccadd4cc605fb40683818fccf99b04
Jasmine-mai <>
2024-03-16 21:11:09 +0700

    Jasmine add exercise

--- jasmine99.js
+++ jasmine99.js
@@ -1,1 +1,9 @@
 //jasmine180699
+
+let maleEmployeeNumber = 50
+let femaleEmployeeNumber = 15
+let totalEmployeeNumber = maleEmployeeNumber + femaleEmployeeNumber
+console.log("total member " + totalEmployeeNumber) 
+
+
+
