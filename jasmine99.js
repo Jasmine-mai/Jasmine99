740951fd6bb6aba8eaee74a7e80aa2c79973157f
Jasmine-mai <>
2024-03-17 11:51:40 +0700

    Jasmine update function

--- jasmine99.js
+++ jasmine99.js
@@ -7,9 +7,14 @@ let totalEmployeeNumber = maleEmployeeNumber + femaleEmployeeNumber
 
 let a = 5, b = 10
 let c = a++ * --b + --a * b++
-console.log("result " + c)
+//console.log("result " + c)
 // 5 * --b + --a * b++ // a=6, b+10
 // 5 * 9 + --a * b ++ // a=6, b=9
 // 5 * 9 + 5 * 9 = 45 + 45= 90
 
+function calculateDiscSquare(r) {
+    return r * r * 3.14;
+}
+//console.log("result " + calculateDiscSquare(10))
+console.log("result " + calculateDiscSquare(15))
 
