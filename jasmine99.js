e68ee01cc9459d2ee3777324c781f582abf22075
Jasmine-mai <>
2024-03-17 10:06:09 +0700

    Jasmine update operators

--- jasmine99.js
+++ jasmine99.js
@@ -3,7 +3,13 @@
 let maleEmployeeNumber = 50
 let femaleEmployeeNumber = 15
 let totalEmployeeNumber = maleEmployeeNumber + femaleEmployeeNumber
-console.log("total member " + totalEmployeeNumber) 
+//console.log("total member " + totalEmployeeNumber) 
 
+let a = 5, b = 10
+let c = a++ * --b + --a * b++
+console.log("result " + c)
+// 5 * --b + --a * b++ // a=6, b+10
+// 5 * 9 + --a * b ++ // a=6, b=9
+// 5 * 9 + 5 * 9 = 45 + 45= 90
 
 
